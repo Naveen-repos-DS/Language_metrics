75f6ea88be3835091bf67b8f21f823ba74ab3f01
Naveen <imnaveen0030@gmail.com>
2026-02-09 19:50:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import sys
 from deep_translator import GoogleTranslator
-from pandas as pd
+import pandas as pd
 from comet import download_model,load_from_checkpoint
 
 text = st.text_input("Enter text")
@@ -11,6 +11,15 @@ st.write(translated_text)
 
 
 model_path = download_model("Unbabel/wmt22-comet-da")
+model = load_from_checkpoint(model_path)
+data = dict(src=text,mt=translated_text,ref=translated_text)
+pred = model.predict([data])
+comet_pred = pred[1]*100;comet_pred
+out = dict(COMET_Score = comet_pred)
+metrics = pd.DataFrame(out,index=[0])
+st.write(metrics)
+
+
 
 
 
