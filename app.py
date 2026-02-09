8badbc094d5bf36962bf07e7068b6b918052d7b5
Naveen <imnaveen0030@gmail.com>
2026-02-09 19:47:57 +0530

    Import pandas and download translation model
    Added model download functionality and pandas import.

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 import streamlit as st
 import sys
 from deep_translator import GoogleTranslator
+from pandas as pd
+from comet import download_model,load_from_checkpoint
 
 text = st.text_input("Enter text")
 translator = GoogleTranslator(source="auto", target="fr")
 translated_text = translator.translate(text)
 st.write(translated_text)
+
+
+model_path = download_model("Unbabel/wmt22-comet-da")
+
+
+
+
+
+
