992df3d5f37b3c7f6c9d8ed47138b0721fcaf92f
Naveen <imnaveen0030@gmail.com>
2026-02-09 19:59:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,16 @@ translator = GoogleTranslator(source="auto", target="fr")
 translated_text = translator.translate(text)
 st.write(translated_text)
 
+@st.cache_resource
+def load_comet_model():
+    from comet import download_model, load_from_checkpoint
+    model_path = download_model("Unbabel/wmt22-comet-da")
+    return load_from_checkpoint(model_path)
 
-model_path = download_model("Unbabel/wmt22-comet-da")
-model = load_from_checkpoint(model_path)
+model = load_comet_model()
 data = dict(src=text,mt=translated_text,ref=translated_text)
 pred = model.predict([data])
-comet_pred = pred[1]*100;comet_pred
+comet_pred = pred[1]*100
 out = dict(COMET_Score = comet_pred)
 metrics = pd.DataFrame(out,index=[0])
 st.write(metrics)
